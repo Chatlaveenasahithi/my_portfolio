f1bb3ee95291114326254a5b8c7e0acaa2476c1d
Chatlaveenasahithi <132035102+Chatlaveenasahithi@users.noreply.github.com>
2023-10-28 21:50:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// Event Listeners: Handling toggle event
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+// Save user preference on load
+
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+// Adding date
+let myDate = document.querySelector("#datee");
+const yes = new Date().getFullYear();
+myDate.innerHTML = yes;
+
+function downloadResume() {
+  // Create a link element to trigger the download
+  var downloadLink = document.createElement("a");
+
+  // Use forward slashes or double backslashes in the file path
+  downloadLink.href = "Resume.pdf"; // Update with the correct path
+  downloadLink.download = "Resume.pdf"; // Specify the desired filename for the downloaded file
+
+  // Trigger a click event on the link to initiate the download
+  downloadLink.click();
+}
+
